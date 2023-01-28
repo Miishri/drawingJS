68614d7e4867ffb2bc883f2aa9c3ee8c9c07c5b2
school laptop <muhammad.shamaeem@elev.ga.ntig.se>
2023-01-28 16:42:42 +0100

    ADD drawing functionality

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,12 @@ const body = document.querySelector("body");
 function makeGrid(size = 2) { //make one square box
     const grid = document.createElement("div");
     grid.classList.add("grid");
+    //listen to color
+    grid.addEventListener("mouseover", () => {
+        if (grid.style.background !== "blue") {
+            grid.style.background = "blue";
+        }
+    });
     grid.setAttribute("style", `border: 1px solid black`);
     container.setAttribute("style", `grid-template-row: repeat(${size}, 1fr); grid-template-columns: repeat(${size}, 1fr);`);
     container.appendChild(grid);
