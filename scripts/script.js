1df358d1b3bfbd5088cb48a2b11a8238d8db7e77
school laptop <muhammad.shamaeem@elev.ga.ntig.se>
2023-01-28 15:46:59 +0100

    ADD putting grids into container FUNCTION

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,21 @@
 const container = document.querySelector(".container");
 const body = document.querySelector("body");
 
-function makeGrid(size = "2") { //make one square box
+function makeGrid(size = 2) { //make one square box
     const grid = document.createElement("div");
     grid.classList.add("grid");
     grid.setAttribute("style", `border: 1px solid black`);
     container.setAttribute("style", `grid-template-row: repeat(${size}, 1fr); grid-template-columns: repeat(${size}, 1fr);`);
     container.appendChild(grid);
-}   
+}  
+
+function putGrids(size = 2) { //make 2 x 2 by default or given size
+    container.querySelectorAll("div").forEach(d => d.remove());
+    
+    for (let i = 0; i < size; i++) {
+        makeGrid(size);
+        for (let j = 1; j < size; j++) {
+            makeGrid(size);
+        }
+    }
+}
